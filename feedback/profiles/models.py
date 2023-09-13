0d24db47bcb5dae5d0bc0b6f7a8759efbcec6c58
Paulina Kimak <paulina.kimak.oferty@gmail.com>
2023-09-13 05:56:41 +0200

    'RockPiryt' - add new feature, user can upload images,
    - create new app for users profiles,
    - user can upload images,
    - images(path) are saved in database,

--- feedback/profiles/models.py
+++ feedback/profiles/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class UserProfile(models.Model):
+    zimage = models.ImageField(upload_to="user_images")
+    # zimage = models.FileField(upload_to="user_images")
--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponseRedirect
+from .forms import ProfileForm
+from .models import UserProfile
+from django.views.generic.edit import CreateView
 
 # Create your views here.
+################################Auto Form from model with CreateView
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile_createview.html"
+    model = UserProfile
+    fields = "__all__"
+    success_url = "/profiles"
+#
+################################DjangoForm+store file (path) in Database
+# class CreateProfileView(View):
+#     def get(self, request):
+#         image_form = ProfileForm()
+#         return render(request, "profiles/create_profile.html", {
+#             "html_image_form":image_form,
+#         })
+
+#     def post(self, request):
+#         submitted_form = ProfileForm(request.POST, request.FILES)
+#         if submitted_form.is_valid():
+#             profile = UserProfile(zimage=request.FILES["user_image"])
+#             profile.save()
+#             return HttpResponseRedirect("/profiles")
+        
+#         return render(request, "profiles/create_profile.html", {
+#             "html_image_form":submitted_form,
+#         })
+
+#################################html form with name="my_image"
+# def store_file(file):
+#     with open("temp/image.jpg", "wb+") as destination:
+#         for chunk in file.chunks():
+#             destination.write(chunk)
+# class CreateProfileView(View):
+#     def get(self, request):
+#         return render(request, "profiles/create_profile.html")
+
+#     def post(self, request):
+#         store_file(request.FILES["my_image"])
+#         return HttpResponseRedirect("/profiles")
